bb49babfcd3163758f8e2b75c09553efea5e7ed8
Anthony Giretti <Anthony.Giretti@equisoft.com>
2019-04-23 19:06:17 -0400

    create dependency injection project

---
+++ Plugins.DependencyInjection.AspNetCore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Plugins.Tools;
+using Plugins.Tools.Extensions;
+
+namespace Plugins.DependencyInjection.AspNetCore.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers in DI a delegate that finds in <see cref="IContainer"/> the correct instance for a given Interface and tenant name
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <param name="services"><see cref="IServiceCollection"/> to register plugin delegates</param>
+        /// <param name="getContainer"><see cref="Func{T}"/> that returns <see cref="IContainer"/></param>
+        /// <param name="pluggableItems">Collection of <see cref="PluggableItem"/> to be filtered by a tenant key and Interface</param>
+        public static void RegisterPluginDelegate<TInterface>(this IServiceCollection services, Func<IContainer> getContainer, IEnumerable<PluggableItem> pluggableItems)
+        {
+            // Prefilter by interface at startup
+            var filteredItems = pluggableItems.FilterByInterface(typeof(TInterface));
+            
+            services.AddScoped<Func<string, TInterface>>(serviceProvider => (tenant) =>
+            {
+                var items = filteredItems.FilterByTenant(tenant);
+
+                if (!items.Any())
+                {
+                    throw new KeyNotFoundException($"No instance found for the tenant '{tenant}'.");
+                }
+
+                if (items.Count() > 1)
+                {
+                    throw new AmbiguousMatchException($"Several instances have been found for the tenant '{tenant}'.");
+                }
+
+                return (TInterface)serviceProvider.GetService(items.First().Interface);
+
+            });
+        }
+    }
+}
---
+++ Plugins.DependencyInjection.AspNetCore/Providers/PluginProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Plugins.Abstractions.Providers;
+using System;
+
+namespace Plugins.DependencyInjection.AspNetCore.Providers
+{
+    /// <inheritdoc/>
+    public class PluginProvider<TInterface> : IPluginProvider<TInterface>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public PluginProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <inheritdoc/>
+        public TInterface GetInstance(string key)
+        {
+            var serviceFunc = this.GetServiceFunction();
+            return serviceFunc(key);
+        }
+
+        /// <summary>
+        /// Gets the delegate for a given pair of key / interface
+        /// </summary>
+        /// <returns><see cref="Func{T1, TInterface}"/></returns>
+        private Func<string, TInterface> GetServiceFunction()
+        {
+            return (Func<string, TInterface>)_httpContextAccessor.HttpContext.RequestServices.GetRequiredService(typeof(Func<string, TInterface>));
+        }
+    }
+}
